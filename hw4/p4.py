1420b0c5c9fc0549332bf04cc69197d89092be93
wukm <lwukmer@gmail.com>
2015-11-26 13:55:59 -0800

    fixed cropping function bug; ignore swp files

--- hw4/p4.py
+++ hw4/p4.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import numpy as np
 from scipy.fftpack import fft2, ifft2, fftshift, ifftshift, fftfreq
 import PIL.Image
@@ -10,31 +9,72 @@ import matplotlib.cm as cm
 from mpl_toolkits.mplot3d import Axes3D
 
 
-IMAGE_FILE = "./lena_noisy.png"
-
 def crop_range(A, border_val=255):
     """
-    returns a slice such that A[ind] is a matrix with a border removed.
-    that is, these indices indicate the 'subimage' part of A.
-    be careful to round first, since border is identified by equality with
-    border_val
-    """
-    # this is needlessly wasteful. only need the first and last one of each
-    # maybe mask out the border_val and just find argmax, argmin for each axis
-    a, b = np.where(A != border_val)
+    returns a slice object that would remove a border about a subimage.
+    That is, these indices indicate the 'subimage' part of A. See example below. 
+    Note that this border_val is found via equality only.
     
+    For now, A must be a 2D array. 
+
+    INPUT
+    A:   a 2D numpy.array
+    border_val: the "border val" to be cropped out (default 255)
+    
+    OUTPUT:
+    s: a slice object such that A[s] is the subimage.
+
+    Example:
+
+        A = np.array([[0, 1, 2, 0, 0],
+                    [0, 0, 1, 3, 0],
+                    [0, 1, 3, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0]])
+
+        s = crop_range(A, 0)
+
+        A[s]
+        >>> array([[1, 2, 0],
+                [0, 1, 3],
+                [1, 3, 0]])
+
+    """
+
+    # can rewrite for arbitrary dim: store in a N x 2 array and iterate, but
+    # making slice object needs some thought
+    # note this current implementation may be inefficient for large matrices
+
+    for i_start in range(A.shape[0]):
+        if not all(A[i_start] == border_val):
+            break
+
+    for i_end in range(-1, -A.shape[0] -1, -1):
+        if not all(A[i_end] == border_val):
+            break
+    
+    for j_start in range(A.shape[1]):
+        if not all(A[:,j_start] == border_val):
+            break
+
+    for j_end in range(-1, -A.shape[1] - 1, -1):
+        if not all(A[:,j_end] == border_val):
+            break
+
     # return a slice object S such that A[S] is the subimage
-    return np.s_[a[0]:a[-1]+1 , b[0]:b[-1]+1]
+    return np.s_[i_start:i_end+1 , j_start:j_end+1]
 
 def gaussian(point, sigma, center=None):
     """
     the 2D gaussian of points x, y = point
     with variance sigma and center a,b = center (defaults to (0,0))
     """
+
     if center is not None:
         a, b = center
     else:
         a, b = 0, 0
+
     x, y = point
     res = (x-a)**2 + (y-b)**2
     res = (-res)/(2*(sigma**2))
@@ -42,32 +82,21 @@ def gaussian(point, sigma, center=None):
 
     return res
 
-#def gaussian_filter(shape, sigma, center=None):
-#    """
-#    shape is shape of image you wish to filter
-#    """
-#    
-#    # zero centered integer steps the same shape as the original image 
-#    mesh_x = np.arange(shape[0], dtype=np.float) - shape[0]//2
-#    mesh_y = np.arange(shape[1], dtype=np.float) - shape[1]//2
-#    
-#    step_scale = sigma * np.sqrt(2)
-#    mesh_x /= step_scale
-#    mesh_y /= step_scale 
-#
-#    mesh = np.meshgrid(mesh_y, mesh_x)
-#    
-#    g_kern = gaussian(mesh, sigma)
-#
-#    k = np.sqrt( 2*np.pi) * sigma
-#    g_kern /= k
-#
-#    assert g_kern.shape == shape
-#    #g_kern /= g_kern.sum()
-#    # return mesh for graphing purposes
-#    return g_kern, mesh
+def gaussian_filter(shape, sigma):
+    """
+    creates a discrete gaussian filter of shape `shape` with variance `sigma`
+    centered in the middle. Note that this filter is not normalized in any way.
+    
+    INPUT:
+    
+    shape:  a tuple representing the size of the filter
+    sigma:  variance for the gausian function (a scalar)
+    
+    OUTPUT:
 
-def gaussian_filter(shape, sigma, center=None):
+    G:  a discrete gaussian filter with G.shape == `shape`
+
+    """
     
     M, N = shape
     mesh_x = np.arange(M)
@@ -81,58 +110,62 @@ def gaussian_filter(shape, sigma, center=None):
 
     return g_kern, mesh
 
-img_raw = PIL.Image.open(IMAGE_FILE)
-img_raw = img_raw.convert('L') # convert to grayscale, all channels are equal
-img = np.array(img_raw, dtype='f')
 
-fs = fft2(img)
-fss = fftshift(fs)
-g_kern, g_mesh = gaussian_filter(img.shape, sigma=15)
+if __name__ == "__main__":
 
-convolute = g_kern * fss
-filtered = ifft2(ifftshift(convolute))
+    img_raw = PIL.Image.open('./lena_noisy')
 
-print('imaginary error accumulated:')
-print('\t np.abs(filtered.imag).max()=', np.abs(filtered.imag).max())
-f_img = filtered.real
+    img_raw = img_raw.convert('L') # convert to grayscale, all channels are equal
+    img = np.array(img_raw, dtype='f')
 
-# actually, any nonreal components must be from roundoff
-mag = np.log10(np.abs(fss))
+    fs = fft2(img)
+    fss = fftshift(fs)
+    g_kern, g_mesh = gaussian_filter(img.shape, sigma=15)
 
-mag2 = np.log10(np.abs(convolute))
+    convolute = g_kern * fss
+    filtered = ifft2(ifftshift(convolute))
 
-# make a 2x2 plot and give all the subaxes names for ease
+    print('imaginary error accumulated:')
+    print('\t np.abs(filtered.imag).max()=', np.abs(filtered.imag).max())
+    f_img = filtered.real
 
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
+    # actually, any nonreal components must be from roundoff
+    mag = np.log10(np.abs(fss))
 
+    mag2 = np.log10(np.abs(convolute))
 
-ax1.imshow(img, cmap=cm.Greys_r)
-ax2.imshow(mag) # default (nongrey) colormap is actualy easier to see
-ax3.imshow(mag2)
-ax4.imshow(f_img, cmap=cm.Greys_r)
+    # make a 2x2 plot and give all the subaxes names for ease
 
-fig.show()
-
-fig2 = plt.figure()
-bx1 = fig2.add_subplot(221, projection='3d')
-bx1.plot_surface(g_mesh[0], g_mesh[1], g_kern)
-
-bx2 = fig2.add_subplot(222)
-
-bx2.imshow(g_kern)
-
-bx3 = fig2.add_subplot(223)
-bx4 = fig2.add_subplot(224)
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
 
 
-# make histograms of the cropped region for the original image and the result
-S = crop_range(img)
-img_crop = img[S].round()
-filtered_crop = f_img[S].round()
+    ax1.imshow(img, cmap=cm.Greys_r)
+    ax2.imshow(mag) # default (nongrey) colormap is actualy easier to see
+    ax3.imshow(mag2)
+    ax4.imshow(f_img, cmap=cm.Greys_r)
 
-orig = np.histogram(img_crop.flatten(), bins=256)[0]
-filt = np.histogram(filtered_crop.flatten(), bins=256)[0]
-bx3.plot(range(256), orig)
-bx4.plot(range(256), filt)
+    fig.show()
 
-fig2.show()
+    fig2 = plt.figure()
+    bx1 = fig2.add_subplot(221, projection='3d')
+    bx1.plot_surface(g_mesh[0], g_mesh[1], g_kern)
+
+    bx2 = fig2.add_subplot(222)
+
+    bx2.imshow(g_kern)
+
+    bx3 = fig2.add_subplot(223)
+    bx4 = fig2.add_subplot(224)
+
+
+    # make histograms of the cropped region for the original image and the result
+    S = crop_range(img)
+    img_crop = img[S].round()
+    filtered_crop = f_img[S].round()
+
+    orig = np.histogram(img_crop.flatten(), bins=256)[0]
+    filt = np.histogram(filtered_crop.flatten(), bins=256)[0]
+    bx3.bar(range(256), orig)
+    bx4.bar(range(256), filt)
+
+    fig2.show()
